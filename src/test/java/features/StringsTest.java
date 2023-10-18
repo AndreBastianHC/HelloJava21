d8cc190704d6a17828390454dc37b98256fd5f7d
AndréBastian <andre.bastian@haeger-consulting.de>
2023-10-18 07:39:12 +0200

    String

--- src/test/java/features/StringsTest.java
+++ src/test/java/features/StringsTest.java
@@ -12,16 +12,16 @@ class StringsTest {
     @Test
     void indexOfTest(){
         String string1 = "Wir suchen den Index eines erstmalig auftretenden Substrings in einem Teilbereich dieses Strings.";
-
         int substringIndex = string1.indexOf("Substring", 0, string1.length()-1);
-        System.out.println(STR."Substring index: \{substringIndex}");
 
         assertEquals(-1, string1.indexOf("Substring", 50, 58));
         assertEquals(-1, string1.indexOf("Substring", 51, 59));
         assertEquals(50, string1.indexOf("Substring", 50, 59));
         assertEquals(50, string1.indexOf("Substring", 23, 69));
+        System.out.println(STR."Substring index: \{substringIndex}");
 
         substringIndex = string1.indexOf("ring", 30, string1.length()-1);
+        assertEquals(55,string1.indexOf("ring", 20, string1.length()-1));
         System.out.println(STR."First Occurence: \{substringIndex}");
 
         substringIndex = string1.indexOf("ring", 60, string1.length()-1);
@@ -29,13 +29,12 @@ class StringsTest {
         System.out.println(STR."Second Occurence: \{substringIndex}");
 
         String string2 = "Wir suchen den Index eines erstmalig auftretenden Characters in einem Teilbereich dieses Strings";
-
         int charIndex = string2.indexOf('C', 0, string2.length()-1);
-        System.out.println(STR."Character Index: \{charIndex}");
 
         assertEquals(-1, string2.indexOf('C', 51, 58));
         assertEquals(-1, string2.indexOf('C', 20, 49));
         assertEquals(50, string2.indexOf('C', 45, 60));
+        System.out.println(STR."Character Index: \{charIndex}");
     }
 
     @Test
@@ -43,12 +42,12 @@ class StringsTest {
         String string = "Wir spalten diesen String in verschiedene Strings";
 
         String[] stringParts = string.splitWithDelimiters("t", 4);
-        System.out.println(STR."4 splits: \{Arrays.stream(stringParts).toList()}");
         assertEquals(List.of("Wir spal","t", "en diesen S", "t", "ring in verschiedene S", "t", "rings"), Arrays.stream(stringParts).toList());
+        System.out.println(STR."4 splits: \{Arrays.stream(stringParts).toList()}");
 
         stringParts = string.splitWithDelimiters("t", 3);
-        System.out.println(STR."3 splits: \{Arrays.stream(stringParts).toList()}");
         assertEquals(List.of("Wir spal","t", "en diesen S", "t", "ring in verschiedene Strings"), Arrays.stream(stringParts).toList());
+        System.out.println(STR."3 splits: \{Arrays.stream(stringParts).toList()}");
     }
 
     @Test
